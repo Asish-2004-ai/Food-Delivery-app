0fc1548e61a1b259707e5b1e15edca627e5d2347
Asish-2004-ai <asishdalabehera375@gmail.com>
2024-10-06 11:23:38 +0530

    changed

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express'
+import { Request, Response } from 'express'
+import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import mongoose from 'mongoose'
@@ -7,7 +8,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import Restaurant from './routes/restaurant'
 import searchRest from './routes/searchRestaurant'
 import Order from './routes/order'
-// import path from 'path'
+import path from 'path'
 
 mongoose.connect(process.env.mongo_url as string).then(()=>{
     console.log("connected to database")
@@ -21,24 +22,24 @@ cloudinary.config({
 
 const app = express()
 
-// const _dirname = path.resolve()
+export const _dirname = path.resolve()
 app.use(cors())
 app.use("/api/order/checkout/webhook",express.raw({type: "*/*"}))
 app.use( express.json())
 
-app.get('/', async(req: Request, res: Response)=>{
-    res.json({message:"Hello hey"})
-});
+// app.get('/', async(req: Request, res: Response)=>{
+//     res.json({message:"Hello hey"})
+// });
 
 app.use('/api/user',userRouter)
 app.use('/api/my/restaurant',Restaurant)
 app.use('/api/restaurants', searchRest)
 app.use('/api/order',Order)
 
-// app.use(express.static(path.join(_dirname, "/frontend/dist")))
-// app.get("*", (req: Request, res: Response)=>{
-//     res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
-// })
+app.use(express.static(path.join(_dirname, "/Frontend/dist")))
+app.get("*", (req: Request, res: Response)=>{
+    res.sendFile(path.resolve(_dirname, "Frontend", "dist", "index.html"))
+})
 
 app.listen(1000,()=>{
     console.log("server started")
